69df348e67f05402df502c3fc5a037c191859951
Syahrifal Dani S <43494734+SyahrifalD@users.noreply.github.com>
2018-10-08 14:21:58 +0700

    Add files via upload

---
+++ PBO3-10117093-latihan40-oOwarnaKepribadian/src/pbo/pkg10117093/latihan40/oowarnakepribadian/PBO10117093Latihan40OOwarnaKepribadian.java
@@ -0,0 +1,26 @@
+/*
+ * NAMA     : Syahrifal Dani
+ * Kelas    : IF - 3
+ * NIM      : 10117093
+ * Deskripsi Program  : Program ini berisi program untuk menampilkan Warna Ke 
+ * pribadian dengan menggunakan dua class
+ */
+package pbo.pkg10117093.latihan40.oowarnakepribadian;
+
+/**
+ *
+ * @author Syahrifal
+ */
+public class PBO10117093Latihan40OOwarnaKepribadian {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          Warna skuyMu = new Warna();
+        
+        skuyMu.ioWarnaMu();
+    }
+    
+}
