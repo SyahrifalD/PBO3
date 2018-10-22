21635fa82703dbea5c45ef7dddbb5145ca31ee41
Syahrifal Dani S <43494734+SyahrifalD@users.noreply.github.com>
2018-10-22 19:35:49 +0700

    Add files via upload

---
+++ PBO3-10117093-latihan49-biayaEmasKawin/src/pbo3/pkg10117093/latihan49/biayaemaskawin/PBO310117093Latihan49BiayaEmasKawin.java
@@ -0,0 +1,48 @@
+/*
+ * NAMA     : Syahrifal Dani
+ * Kelas    : IF - 3
+ * NIM      : 10117093
+ * Deskripsi Program  : menapilkan program biaya emas
+ */
+package pbo3.pkg10117093.latihan49.biayaemaskawin;
+
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+/**
+ *
+ * @author Syahrifal
+ */
+public class PBO310117093Latihan49BiayaEmasKawin {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        DecimalFormat mataUangIndonesia = (DecimalFormat) 
+                      DecimalFormat.getCurrencyInstance();
+        DecimalFormatSymbols formatRP = new DecimalFormatSymbols();
+        
+        formatRP.setCurrencySymbol("Rp.");
+        formatRP.setMonetaryDecimalSeparator(',');
+        formatRP.setGroupingSeparator('.');
+        
+        mataUangIndonesia.setDecimalFormatSymbols(formatRP);
+        
+        
+        Emas objEmas = new Emas();
+        objEmas.setHargaGram(570000);
+        objEmas.setBeratEmas(15.7);
+        System.out.println("====Aplikasi Hitung Harga Emas====");
+        System.out.printf("Total Bayar Emas : %s %n ", 
+                           mataUangIndonesia.format(
+                           objEmas.perhitungan(objEmas.getHargaGram(), 
+                           objEmas.getBeratEmas())));
+    
+        
+    
+    }
+    
+}
+
---
+++ PBO3-10117093-latihan50-energiKinetik/src/pbo3/pkg10117093/latihan50/energikinetik/PBO310117093Latihan50EnergiKinetik.java
@@ -0,0 +1,39 @@
+/*
+ * NAMA     : Syahrifal Dani
+ * Kelas    : IF - 3
+ * NIM      : 10117093
+ * Deskripsi Program  : menapilkan energi kinetik
+ */
+package pbo3.pkg10117093.latihan50.energikinetik;
+
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author Syahrifal
+ */
+public class PBO310117093Latihan50EnergiKinetik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   Energi kinetik = new Energi();
+        kinetik.setMassa(0.145);
+        kinetik.setKecepatan(25);
+        kinetik.setKecepatanAwal(0);
+        kinetik.setKecepatanAkhir(25);
+        
+        DecimalFormat df = new DecimalFormat("#.#");
+        
+        System.out.println("=====PROGRAM MENCARI ENERGI KINETIK=====");
+        System.out.println("Energi Kinetik : " + df.format(
+                           kinetik.hitungEnergiKinetik
+                          (kinetik.getMassa(), kinetik.getKecepatan())));
+        System.out.println("Usaha : " + df.format (kinetik.hitungUsaha
+                          (kinetik.getKecepatanAwal(), 
+                           kinetik.getKecepatanAkhir())));
+    }
+    
+}
